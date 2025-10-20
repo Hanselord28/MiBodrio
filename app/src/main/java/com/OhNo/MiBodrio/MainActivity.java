15d6b132868deaf6124ead3744b74a177db35da5
hansel <Hanselord16gmail.com>
2025-10-20 15:43:31 -0300

    integrando...

--- app/src/main/java/com/OhNo/MiBodrio/MainActivity.java
+++ app/src/main/java/com/OhNo/MiBodrio/MainActivity.java
@@ -1,14 +1,38 @@
 package com.OhNo.MiBodrio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.net.wifi.hotspot2.pps.HomeSp;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics oFirebaseAnalytics;
+    private FirebaseAuth oFirebaseAuth;
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        oFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("Mensaje", "Entro_al_log");
+        oFirebaseAnalytics.logEvent("pantalla_de_logeo", bundle);
+        this.setTitle("Inicio de sesión");
     }
 }
